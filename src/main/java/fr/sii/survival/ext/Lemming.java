391b350963e5c7463a1eeda06318026db442b81f
abaudet <abaudet@sii.fr>
2015-04-02 13:44:55 +0200

    Update javadoc

--- src/main/java/fr/sii/survival/ext/Lemming.java
+++ src/main/java/fr/sii/survival/ext/Lemming.java
@@ -3,7 +3,6 @@ package fr.sii.survival.ext;
 import java.util.concurrent.TimeUnit;
 
 import fr.sii.survival.core.domain.image.ClientImage;
-import fr.sii.survival.core.domain.player.Wizard;
 import fr.sii.survival.core.ext.DelegateEnemyExtension;
 import fr.sii.survival.core.ext.GameContext;
 import fr.sii.survival.core.ext.annotation.Developer;
@@ -14,7 +13,6 @@ import fr.sii.survival.core.ext.behavior.move.EnemyMoveBehavior;
 import fr.sii.survival.core.ext.behavior.move.RandomMoveNearBehavior;
 import fr.sii.survival.core.ext.behavior.target.RandomPlayerTargetBehavior;
 import fr.sii.survival.core.ext.behavior.target.TargetBehavior;
-import fr.sii.survival.core.service.game.PlayerTypePredicate;
 
 /**
  * Simple enemy that attack a random alive player every 2 seconds. He attacks
@@ -23,11 +21,16 @@ import fr.sii.survival.core.service.game.PlayerTypePredicate;
  * @author Aurélien Baudet
  *
  */
-@Developer(value = "abaudet", name = "Aurélien Baudet", email = "abaudet@sii.fr")
+@Developer(value="abaudet", name="Aurélien Baudet", email="abaudet@sii.fr")
 public class Lemming extends DelegateEnemyExtension {
 
 	/**
-	 * Lemmings are small rodents, usually found in or near the Arctic, in tundra biomes. They are subniveal animals, and together with voles and muskrats, they make up the subfamily Arvicolinae (also known as Microtinae), which forms part of the largest mammal radiation by far, the superfamily Muroidea, which also includes rats, mice, hamsters, and gerbils.
+	 * Lemmings are small rodents, usually found in or near the Arctic, in
+	 * tundra biomes. They are subniveal animals, and together with voles and
+	 * muskrats, they make up the subfamily Arvicolinae (also known as
+	 * Microtinae), which forms part of the largest mammal radiation by far, the
+	 * superfamily Muroidea, which also includes rats, mice, hamsters, and
+	 * gerbils.
 	 * 
 	 * @return a lemming aka the weakest enemy in the world
 	 */
@@ -39,9 +42,9 @@ public class Lemming extends DelegateEnemyExtension {
 	/**
 	 * Chose a Random Target (Enemy or Wizard (human players are wizards))
 	 * 
-	 * @param  context
-	 *           the game context
-	 * @return         new RandomPlayerTargetBehavior()
+	 * @param context
+	 *            the game context
+	 * @return any player on the game chosen randomly
 	 */
 	@Override
 	protected TargetBehavior getTargetBehavior(GameContext context) {
@@ -49,11 +52,12 @@ public class Lemming extends DelegateEnemyExtension {
 	}
 
 	/**
-	 * Lemming will attack its target (randomly chosen each action) every 2 seconds doing 20 health points damages
+	 * Lemming will attack its target (randomly chosen each action) every 2
+	 * seconds doing 20 health points damages
 	 * 
-	 * @param  context
-	 *           the game context
-	 * @return         AttackActionBehavior for 20 HP with a 2 seconds cooldown
+	 * @param context
+	 *            the game context
+	 * @return an action that attacks for 20 HP with a 2 seconds cooldown
 	 */
 	@Override
 	protected EnemyActionBehavior getActionBehavior(GameContext context) {
@@ -63,9 +67,9 @@ public class Lemming extends DelegateEnemyExtension {
 	/**
 	 * Randomly moves by one case horizontally or vertically (exclusive)
 	 * 
-	 * @param  context 
-	 *           the game context
-	 * @return         RandomMoveNearBehavior
+	 * @param context
+	 *            the game context
+	 * @return the enemy will move either one case (either up, down, left or right)
 	 */
 	@Override
 	protected EnemyMoveBehavior getMoveBehavior(GameContext context) {
