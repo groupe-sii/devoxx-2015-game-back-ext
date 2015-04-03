d6dbe4bfb17a6d5ee5c4b9acb6f167ff8f0113bf
cdejonghe <cdejonghe@sii.fr>
2015-04-03 14:02:31 +0200

    commented unvalid Assassin

--- src/main/java/fr/sii/survival/ext/cdejonghe/Assassin.java
+++ src/main/java/fr/sii/survival/ext/cdejonghe/Assassin.java
@@ -1,13 +1,6 @@
 package fr.sii.survival.ext.cdejonghe;
 
-import fr.sii.survival.core.domain.image.ClientImage;
-import fr.sii.survival.core.exception.GameException;
-import fr.sii.survival.core.ext.DelegateEnemyExtension;
-import fr.sii.survival.core.ext.GameContext;
 import fr.sii.survival.core.ext.annotation.Developer;
-import fr.sii.survival.core.ext.behavior.action.EnemyActionBehavior;
-import fr.sii.survival.core.ext.behavior.move.EnemyMoveBehavior;
-import fr.sii.survival.core.ext.behavior.target.TargetBehavior;
 
 
 /**
@@ -18,28 +11,31 @@ import fr.sii.survival.core.ext.behavior.target.TargetBehavior;
  *
  */
 @Developer(value = "cdejonghe", name = "Cyril Dejonghe", email = "cdejonghe@sii.fr")
-public class Assassin /*extends DelegateEnemyExtension */{
+public class Assassin {
 
 	protected Assassin() {
-		//super("Assassin", new ClientImage("assasin"), 10);
+		// super("Assassin", new ClientImage("assasin"), 10);
 	}
 
-	@Override
-	protected EnemyActionBehavior getActionBehavior(GameContext context) throws GameException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected EnemyMoveBehavior getMoveBehavior(GameContext context) throws GameException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected TargetBehavior getTargetBehavior(GameContext context) throws GameException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	// @Override
+	// protected EnemyActionBehavior getActionBehavior(GameContext context)
+	// throws GameException {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
+	//
+	// @Override
+	// protected EnemyMoveBehavior getMoveBehavior(GameContext context) throws
+	// GameException {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
+	//
+	// @Override
+	// protected TargetBehavior getTargetBehavior(GameContext context) throws
+	// GameException {
+	// // TODO Auto-generated method stub
+	// return null;
+	// }
 
 }
