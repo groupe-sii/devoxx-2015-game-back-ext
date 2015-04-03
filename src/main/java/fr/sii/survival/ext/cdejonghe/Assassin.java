17941a0b7ed8f8c95ab15a05209c7affc584d157
cdejonghe <cdejonghe@sii.fr>
2015-04-03 09:46:45 +0200

    added empty Assassin Enemy

--- src/main/java/fr/sii/survival/ext/cdejonghe/Assassin.java
+++ src/main/java/fr/sii/survival/ext/cdejonghe/Assassin.java
@@ -1,15 +1,22 @@
 package fr.sii.survival.ext.cdejonghe;
 
+import fr.sii.survival.core.domain.image.ClientImage;
+import fr.sii.survival.core.ext.DelegateEnemyExtension;
 import fr.sii.survival.core.ext.annotation.Developer;
+
 
 /**
  * Enemy that stalks a player until he is close enough to poisonned it and then
  * chooses another target.
  * 
- * @author Aurélien Baudet
+ * @author Cyril Dejonghe
  *
  */
-@Developer(value = "abaudet", name = "Aurélien Baudet", email = "abaudet@sii.fr")
-public class Assassin {
+@Developer(value = "cdejonghe", name = "Cyril Dejonghe", email = "cdejonghe@sii.fr")
+public class Assassin extends DelegateEnemyExtension {
+
+	protected Assassin() {
+		super("Assassin", new ClientImage("assasin"), 10);
+	}
 
 }
